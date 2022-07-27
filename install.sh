a0ada04636809a92243d76c2b6a666912e433649
getb1 <78261811+getb1@users.noreply.github.com>
2022-07-27 09:31:50 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,3 +6,5 @@ sudo apt upgrade
 sudo apt-get install gcc-4.7 g++-4.7
 sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.6 60 --slave /usr/bin/g++ g++ /usr/bin/g++-4.6
 sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.7 40 --slave /usr/bin/g++ g++ /usr/bin/g++-4.7
+
+sudo chmod +x automatedCompileForC.sh
