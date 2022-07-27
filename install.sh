54e1d198e37dffa10b2c59430e4b8552bb40bf22
getb1 <78261811+getb1@users.noreply.github.com>
2022-07-27 10:01:32 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,8 +3,12 @@
 sudo apt update -y
 sudo apt upgrade -y
 
-sudo apt-get install gcc-4.7 g++-4.7
-sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.6 60 --slave /usr/bin/g++ g++ /usr/bin/g++-4.6
-sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.7 40 --slave /usr/bin/g++ g++ /usr/bin/g++-4.7
+#sudo apt-get install gcc-4.7 g++-4.7
+#sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.6 60 --slave /usr/bin/g++ g++ /usr/bin/g++-4.6
+#sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.7 40 --slave /usr/bin/g++ g++ /usr/bin/g++-4.7
 
 sudo chmod +x automatedCompileForC.sh
+clear
+
+echo "Running Scripts"
+bash automatedCompileForC.sh hello-world.c hello-world
